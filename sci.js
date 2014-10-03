ab7c521f7bc31bc9e77011c1ef090f451866fe00
Aaron Greenwald <aaron@aarongreenwald.com>
2014-10-02 22:17:13 -0400

    better error handling and logging, simplified code
    There's still a lot to do here with the logging,
    but this is a step in the right direction.
    We also need better error checking.

--- sci.js
+++ sci.js
@@ -2,60 +2,124 @@ var fs = require('fs')
 var http = require("http")
 var spawn = require('child_process').spawn
 
-//repositories.json is a json formatted array of objects of form:
-//{ "name" : "aarongreenwald/sci", "scriptDirectory" : "some/relative/or/absolute/path", "script": "scriptName.sh"}
-var repositories = JSON.parse(fs.readFileSync('sci/repositories.json', 'utf8'))
-
-var handleError = function(ex, response){
-    response.write(ex.message ? ex.message : 'An uknown error occurred')
+var log = function(message){
+    console.log(message)
+    //TODO write to a log file, add datetime and other info
 }
 
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"})
-    try {                     
-         var body = ' '            
-         if (request.method == 'POST') {                 
-            request.on('data', function (data) {
-                body += data                                                  
-            })
-            
-            request.on('end', function () {
-                try {         
-                    try {           
-                        var payload = JSON.parse(body)                              
-                    } catch (ex) {                        
-                        throw { message: 'Unable to parse payload' + body }
-                    }
-                    
-                    var found = false
-                    for (var i = 0; i < repositories.length; i++){
-                        var repository = repositories[i]
-                        if (payload.repository.full_name === repository.name){                    
-                            spawn('sh', [ repository.script ], { 
-                                cwd: repository.scriptDirectory
-                            })           
-                            found = true
-                            break             
-                        } 
-                    }
-                    if (!found){                        
-                        throw 'Invalid repository'                        
-                    }    
-                } catch (ex) {                                        
-                    handleError(ex, response)                    
-                }                     
-                finally {                    
-                    response.end()
-                }                                                          
-            }) 
-            
+
+/*
+ * settings.json looks something like this: 
+{
+    "verboseResponses" : "true",
+    "secretKey" : "sha1=somelonghashstringthatrepresentsyoursecretkey", 
+    "repositories": [
+        {   
+            "name": "aarongreenwald/grouper", 
+            "scriptDirectory": "sci/scripts/", 
+            "script": "grouper.sh"
+        },
+        {   
+            "name": "aarongreenwald/sci", 
+            "scriptDirectory": "sci/scripts/", 
+            "script": "sci.sh"
         }
-        else {
-            throw 'Only POST requests are processed by this server.'        
-        }   
-    }catch (ex) { //we didn't even make it to the callback, so we need to handle the error and close the response here
-        handleError(ex, response)
-        response.end()
-    }   
+    ],
+    "port" : 31242
+}
+* 
+* port will default to 31242 if ommitted
+* verboseSettings will default to false if ommitted
+ */
+
+log('sci server starting...')
+
+try {
+    var settings = JSON.parse(fs.readFileSync('sci/settings.json', 'utf8'))
+} 
+catch (ex){
+    log('Cannot parse settings file: ' + ex)
+    throw ex
+}
+
+var repositories = settings.repositories
+var port = settings.port || 31242
+
+
+log('Listening for POST requests on port ' + port + '...')
+
+http.createServer(function(request, response) {  
     
-}).listen(31242);
+    var handleError = function(ex, response){
+        response.writeHead(500, {"Content-Type": "text/plain"})
+        if (settings.verboseResponses){
+            response.write(ex.message || ex ||  'An uknown error occurred')    
+        }
+        else {        
+            response.write('That didn\'t work. Try again.')        
+        }    
+        log(ex.message || ex || 'An unknown error occurred.') //TODO I should log more than this
+    }
+
+              
+    var body = ' '                                
+    request.on('data', function (data) {
+        body += data                                                  
+    })
+    
+    request.on('end', function () {
+        try {                                     
+            if (request.headers['user-agent'].indexOf('GitHub-Hookshot/') === -1){
+                throw 'This request does not appear to originate from github. If you\'re going to try to break the system, at least try harder.'            
+            }
+                        
+            if (request.headers['x-hub-signature'] !== settings.secretKey){
+                throw 'You don\'t seem to be who you say you are. You need the key to enter!!'
+            }
+            
+            if (request.method !== 'POST') {       
+                throw 'Only POST requests are processed by this server.'         
+            }
+            
+            if (request.headers['x-github-event'] !== 'push'){
+                throw 'Only push events are processed by this server.'
+            }
+            
+            try {           
+                var payload = JSON.parse(body)                              
+            } catch (ex) {                        
+                throw   { 
+                            message: 'Unable to parse payload: ' 
+                                    + body 
+                                    + '\n\nError: ' + ex
+                        }
+            }
+            
+            var found = false
+            for (var i = 0; i < repositories.length; i++){
+                var repository = repositories[i]
+                if (payload.repository.full_name === repository.name){                    
+                    spawn('sh', [ repository.script ], { 
+                        cwd: repository.scriptDirectory
+                    })                                      
+                    found = true
+                    break             
+                } 
+            }
+            if (found){
+                response.writeHead(200, {"Content-Type": "text/plain"})
+                var message = 'Successfully processed continuous integration script for repository: ' + repository.name
+                response.write(message) 
+                log(message) //log more than this
+            }
+            else {                        
+                throw 'Invalid repository name.'                        
+            }                 
+        } catch (ex) {                                        
+            handleError(ex, response)                    
+        }                     
+        finally {                    
+            response.end()
+        }                                                          
+    })                            
+}).listen(port);
