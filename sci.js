2b4ca650b15a9d40895cfb32c83af58eaa887883
Aaron Greenwald <aaron@aarongreenwald.com>
2020-10-04 15:38:48 +0300

    refactor: readability, plus extract secret to env variable

--- sci.js
+++ sci.js
@@ -1,22 +1,14 @@
 const fs = require('fs');
-const http = require("http");
-const https = require("https");
+const http = require('http');
+const https = require('https');
 const spawn = require('child_process').spawn;
 const path = require('path');
 const crypto = require('crypto');
-
-const log = function(message){
-    console.log('---------', new Date(), '---------')
-    console.log(message)
-    console.log()
-    //TODO write to a log file
-}
 
 /*
  * settings.json looks something like this: 
 {
     "verboseResponses" : "true",
-    "secretKey" : "sha1=somelonghashstringthatrepresentsyoursecretkey", 
 	"ssl" : "true",
 	"sslOptions" : {},
     "repositories": [
@@ -40,84 +32,43 @@ const log = function(message){
 
 log('sci server starting...')
 
-try {
-    var settings = JSON.parse(fs.readFileSync(path.join(__dirname, 'sci.config.json')))
-} 
-catch (ex){
-    log('Cannot parse settings file: ' + ex)
-    throw ex
-}
+const settings = loadSettings()
 
-const repositories = settings.repositories
+const {repositories} = settings
 const port = settings.port || 31242
+const secret = process.env.SECRET
 
 const requestListener = (request, response) => {
 
-    const handleError = function (ex, response) {
+    const handleError = (ex, response) => {
         response.writeHead(500, {"Content-Type": "text/plain"})
         if (settings.verboseResponses) {
             response.write(ex.message || ex || 'An unknown error occurred')
         } else {
             response.write('That didn\'t work. Try again.')
         }
-        log(ex.message || ex || 'An unknown error occurred.') //TODO I should log more than this
+        log(ex.message || ex || 'An unknown error occurred.')
     };
 
-
     let body = '';
-    request.on('data', data => {
-        body += data                                                  
-    })
+    request.on('data', data => { body += data })
     
     request.on('end', () => {
         try {
-            if (request.headers['user-agent'].indexOf('GitHub-Hookshot/') === -1){
-                throw 'This request does not appear to originate from github. If you\'re going to try to break the system, at least try harder.'            
-            }
-                        
-	    const key = 'sha1=' + crypto.createHmac('sha1', settings.secretKey).update(body).digest('hex')
-	    const suppliedKey = request.headers['x-hub-signature']
+            validateRequest(request)
 
-            if (suppliedKey !== key){
-                throw 'You don\'t seem to be who you say you are. You need the key to enter!!'
-            }
-            
-            if (request.method !== 'POST') {       
-                throw 'Only POST requests are processed by this server.'         
-            }
-            
-            if (request.headers['x-github-event'] !== 'push'){
-                throw 'Only push events are processed by this server.'
-            }
-            
-            try {
-                var payload = JSON.parse(body)
-            } catch (ex) {
-                throw   { 
-                            message: 'Unable to parse payload: ' 
-                                    + body 
-                                    + '\n\nError: ' + ex
-                        }
-            }
+            const payload = getPayload(body)
 
-            let found = false;
-            for (let i = 0; i < repositories.length; i++){
-                const repository = repositories[i]
-                if (payload.repository.full_name === repository.name){
-                    spawn('sh', [ repository.script ], { 
-                        cwd: repository.scriptDirectory
-                    })                                      
-                    found = true
-                    break             
-                } 
-            }
-            if (found){
+            const repository = repositories.find(r => r.name === payload.repository.full_name)
+            if (repository) {
+                spawn('sh', [ repository.script ], {
+                    cwd: repository.scriptDirectory
+                })
                 response.writeHead(200, {"Content-Type": "text/plain"})
-                const message = 'Successfully processed continuous integration script for repository: ' + repository.name;
-                response.write(message) 
-                log(message) //log more than this
-            }
-            else {                        
+                const message = `Successfully processed continuous integration script for repository: ${repository.name}`;
+                response.write(message)
+                log(message)
+            } else {
                 throw 'Invalid repository name.'                        
             }                 
         } catch (ex) {                                        
@@ -129,7 +80,55 @@ const requestListener = (request, response) => {
     })            
 }
 
-log('Listening for POST requests on port ' + port + '...')
+function loadSettings() {
+    try {
+        return JSON.parse(fs.readFileSync(path.join(__dirname, 'sci.config.json')))
+    }
+    catch (ex){
+        log(`Cannot parse settings file: ${ex}`)
+        throw ex
+    }
+}
+
+function getPayload(body) {
+    try {
+        return JSON.parse(body)
+    } catch (ex) {
+        throw {
+            message: `Unable to parse payload: ${body}\n\nError: ${ex}`
+        }
+    }
+}
+
+function validateRequest(request) {
+    const userAgent = request.headers['user-agent'];
+    if (!userAgent.startsWith('GitHub-Hookshot/')){
+        throw 'This request does not appear to originate from github. If you\'re going to try to break the system, at least try harder.'
+    }
+
+    const key = 'sha1=' + crypto.createHmac('sha1', secret).update(body).digest('hex')
+    const suppliedKey = request.headers['x-hub-signature']
+
+    if (suppliedKey !== key){
+        throw 'Invalid signature.'
+    }
+
+    if (request.method !== 'POST') {
+        throw 'Only POST requests are processed by this server.'
+    }
+
+    if (request.headers['x-github-event'] !== 'push'){
+        throw 'Only push events are processed by this server.'
+    }
+}
+
+function log(message){
+    console.log('---------', new Date(), '---------')
+    console.log(message)
+    console.log()
+}
+
+log(`Listening for POST requests on port ${port}...`)
 
 if (!settings.ssl){
 	http.createServer(requestListener).listen(port)
